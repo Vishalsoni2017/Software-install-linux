fecf8360ba5164d0ccd36ed47a821e14c6951bd2
Vishalsoni2017 <76658874+Vishalsoni2017@users.noreply.github.com>
2024-01-14 01:35:30 +0530

    Update maven.sh

--- maven.sh
+++ maven.sh
@@ -1,4 +1,4 @@
 sudo su
-amazon-linux-extras install java-openjdk11 -y
-wget https://dlcdn.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.zip
+sudo amazon-linux-extras install java-openjdk11 -y
+sudo wget https://dlcdn.apache.org/maven/maven-3/3.9.6/binaries/apache-maven-3.9.6-bin.zip
 unzip apache-maven-3.9.6-bin.zip
