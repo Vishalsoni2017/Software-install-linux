79502bd695a9f60669597c74cb1ed931097f4e85
Vishalsoni2017 <76658874+Vishalsoni2017@users.noreply.github.com>
2024-01-14 23:34:04 +0530

    Create dokcer-compose.sh

---
+++ dokcer-compose.sh
@@ -0,0 +1,3 @@
+curl -SL https://github.com/docker/compose/releases/download/v2.24.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod 777 /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
