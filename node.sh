8bbfef0245d3cbfc5d58c254f58ae7b43a88178d
Vishalsoni2017 <76658874+Vishalsoni2017@users.noreply.github.com>
2024-02-01 14:52:48 +0530

    Update node.sh

--- node.sh
+++ node.sh
@@ -1,5 +1,12 @@
 sudo apt update
 sudo apt install nodejs
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 nvm install node
+
+ami2
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 16
+node -v
+npm -v
+
