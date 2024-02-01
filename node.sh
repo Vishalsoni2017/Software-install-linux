c8175aa4907e22b1958a16cf4f3ac1f496447834
Vishalsoni2017 <76658874+Vishalsoni2017@users.noreply.github.com>
2024-02-01 12:57:40 +0530

    Create node.sh

---
+++ node.sh
@@ -0,0 +1,5 @@
+sudo apt update
+sudo apt install nodejs
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+nvm install node
