5c397dd713f1937176252c9e73b39f690e2d6501
Vishalsoni2017 <76658874+Vishalsoni2017@users.noreply.github.com>
2024-02-15 01:36:38 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,7 @@
 sudo su -
 sudo yum install java-11-amazon-corretto.x86_64 git -y
-sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.83/bin/apache-tomcat-9.0.85.zip
+sudo wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.85/bin/apache-tomcat-9.0.85.zip
+
 sudo unzip apache-tomcat-9.0.85.zip
 sudo chmod 777 -R apache-tomcat-9.0.85
 cd apache-tomcat-9.0.83
