e82310ab35aee014171c5dd89542ce4b3d8822b9
Vishalsoni2017 <76658874+Vishalsoni2017@users.noreply.github.com>
2024-02-15 13:40:09 +0530

    Create eks.sh

---
+++ eks.sh
@@ -0,0 +1,8 @@
+1. curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+
+2. curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.29.0/2024-01-04/bin/darwin/amd64/kubectl
+chmod +x ./kubectl
+
